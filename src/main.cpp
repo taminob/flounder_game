1c39ffb0b42d1ff6c585067eef10fe9cec577e09
Tamino Bauknecht <dev@tb6.eu>
2023-05-14 22:35:56 +0200

    main: Add output manager initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
-int main(int /*argc*/, char* /*argv*/[])
+#include "output/output_manager.h"
+#include "output/cerr_output.h"
+#include "output/cout_output.h"
+#include "output/file_output.h"
+
+#include <chrono>
+#include <iostream>
+
+std::shared_ptr<flounder::OutputManager> createOutputManager()
 {
-    return 0;
+    auto output = std::make_shared<flounder::OutputManager>();
+    auto timestamp = std::chrono::system_clock::now().time_since_epoch().count();
+    output->addOutput(
+     std::make_unique<flounder::FileOutput>(std::to_string(timestamp) + "-flounder_log"));
+    output->addOutput(std::make_unique<flounder::CoutOutput>(), flounder::OutputLevel::user);
+    output->addOutput(std::make_unique<flounder::CerrOutput>(),
+     flounder::OutputLevel::fatal | flounder::OutputLevel::error | flounder::OutputLevel::warning |
+      flounder::OutputLevel::notice);
+    return output;
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+    auto output = createOutputManager();
 }
