a38a4ee2f6d90e91156919aba3dffe3e0d920048
Tamino Bauknecht <dev@tb6.eu>
2023-05-17 22:42:43 +0200

    utils/constants: Add ExitCode enum and quit function

---
+++ src/utils/constants.h
@@ -0,0 +1,14 @@
+#ifndef FLOUNDER_GAME_CONSTANTS_H
+#define FLOUNDER_GAME_CONSTANTS_H
+
+namespace flounder
+{
+enum class ExitCode
+{
+    success = 0,
+    error = 1,
+    invalidArguments = 2,
+};
+} // namespace flounder
+
+#endif // FLOUNDER_GAME_CONSTANTS_H
